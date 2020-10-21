b6c71d72279ac1feb058389907a4bd5cd4629901
ValentinRaychev <valentin.raychev@fdiba.tu-sofia.bg>
2020-10-21 12:47:40 +0000

    exercise4

--- src/exercise4/MinIndex.java
+++ src/exercise4/MinIndex.java
@@ -8,15 +8,27 @@ public class MinIndex {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int[] numbers = new int[5];
-
-        System.out.println("Enter numbers: ");
-        for (int index = 0; index < numbers.length; index++) {
+        int length = input.nextInt();
+        double[] numbers = new double[length];
+        double currentNum = 0.0;
+        int outputIndex = 0; 
+        for (int index = 0; index < length; index++) {
             numbers[index] = input.nextInt();
-        }
+            if (index == 0) {
+              currentNum = numbers[index];
+              outputIndex = index;
+            }
+            else  {
+              if  (numbers[index] < currentNum)  {
+                currentNum = numbers[index];
+                outputIndex = index;
+              }
+              
+            }
 
-        String numbersAsString = Arrays.toString(numbers);
-        System.out.println("Numbers: " + numbersAsString);
+        }
+        System.out.println("Min index: " + outputIndex);
+
     }
 
 }
